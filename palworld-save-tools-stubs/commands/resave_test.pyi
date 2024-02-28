4f2bf9709be096b38eba723c0cab04aa7771401f
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-02-27 18:10:36 -0800

    fix: please be fixed now

--- palworld-save-tools-stubs/commands/resave_test.pyi
+++ palworld-save-tools-stubs/commands/resave_test.pyi
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# This scripts takes a .sav file as input, and runs through the sav > JSON > sav process to ensure that the output is the same as the input.
 
 
 def main() -> None: ...
